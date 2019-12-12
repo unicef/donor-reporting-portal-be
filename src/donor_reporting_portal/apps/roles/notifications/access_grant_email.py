2a5da74a9021430c719de6026d59a8faa87cfa58
Domenico DiNicola <dom.dinicola@gmail.com>
2019-12-12 17:33:34 -0500

    fix tox

--- src/donor_reporting_portal/apps/roles/notifications/access_grant_email.py
+++ src/donor_reporting_portal/apps/roles/notifications/access_grant_email.py
@@ -8,7 +8,8 @@ defaults = {
 
     You have been granted access to UNICEF Donor Reporting Portal.
 
-    Please registry at {{home_link}} and feel free to login in the system, username is your e-mail address provided at the time of account registration.
+    Please registry at {{home_link}} and feel free to login in the system,
+    username is your e-mail address provided at the time of account registration.
 
     Thank you.
     """),
@@ -16,8 +17,8 @@ defaults = {
 
     You have been granted access to UNICEF Donor Reporting Portal.<br/><br/>
 
-    Please registry <a href="{{ home_link }}">here</a><br/><br/>
-    and feel free to login in the system, username is your e-mail address provided at the time of account registration.<br/><br/>
+    Please registry <a href="{{ home_link }}">here</a><br/><br/> and feel free to login in the system,<br/>
+    username is your e-mail address provided at the time of account registration.<br/><br/>
 
     Thank you.
     """,
