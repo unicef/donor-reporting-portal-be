c7dc519fe9be5f05064dd472274fbb8210689c4c
Domenico DiNicola <dom.dinicola@gmail.com>
2019-12-28 08:43:50 -0500

    is superuser

--- src/donor_reporting_portal/api/serializers/userrole.py
+++ src/donor_reporting_portal/api/serializers/userrole.py
@@ -20,7 +20,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'first_name', 'last_name', 'email')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'is_superuser')
 
 
 class UserRoleSerializer(serializers.ModelSerializer):
