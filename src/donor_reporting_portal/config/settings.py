525f47e4350f9763c372b4cc013075d308eb96b2
Domenico DiNicola <dom.dinicola@gmail.com>
2020-01-30 13:56:01 -0500

    health checks

--- src/donor_reporting_portal/config/settings.py
+++ src/donor_reporting_portal/config/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE = (
+    'donor_reporting_portal.apps.core.middleware.HealthCheckMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
