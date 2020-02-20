c54a36c1ad88506bc9e4342ddf82a7e6af1d85a2
Domenico DiNicola <dom.dinicola@gmail.com>
2020-02-20 11:29:42 -0500

    sharepoint groups

---
+++ src/donor_reporting_portal/apps/sharepoint/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import SharePointGroup
+
+
+@admin.register(SharePointGroup)
+class SharePointGroupAdmin(admin.ModelAdmin):
+    search_fields = ('name', )
+    list_display = ('name', )
+    filter_horizontal = ('libraries', )
