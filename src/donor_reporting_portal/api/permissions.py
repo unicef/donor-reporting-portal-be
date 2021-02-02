7518d5c0c3509380e7a1a9551fc6954130991bc6
Domenico <dom.dinicola@gmail.com>
2021-02-02 22:10:39 +0100

    tweak backend to handle pooled donors

--- src/donor_reporting_portal/api/permissions.py
+++ src/donor_reporting_portal/api/permissions.py
@@ -5,27 +5,29 @@ from donor_reporting_portal.apps.report_metadata.models import Donor, SecondaryD
 
 class DonorPermission(permissions.BasePermission):
 
-    def get_donors(self, view, query_paramas):
-        donor = secondary_donor = None
-        donor_info = {'id': 'donor_id', 'code': 'donor_code'}
-        secondary_donor_info = {'id': 'secondary_donor_id', 'code': 'secondary_donor_code'}
+    @staticmethod
+    def _get_instance(view, query_params, user, mapping_dict, model):
+        instance = None
+        for field_name, qs_param in mapping_dict.items():
+            values = view.kwargs.get(qs_param, query_params.get(qs_param, None))
+            values = values.split(',') if values else []
+            filter_dict = {field_name + '__in': values, 'roles__user__pk': user.pk}
+            if not instance:
+                instance = model.objects.filter(**filter_dict).first()
+        return instance
 
-        for field_name, qs_param in donor_info.items():
-            value = view.kwargs.get(qs_param, query_paramas.get(qs_param, None))
-            filter_dict = {field_name: value}
-            if not donor:
-                donor = Donor.objects.filter(**filter_dict).first()
+    def _get_donor(self, view, query_params, user):
+        mapping_dict = {'id': 'donor_id', 'code': 'donor_code'}
+        return self._get_instance(view, query_params, user, mapping_dict, Donor)
 
-        for field_name, qs_param in secondary_donor_info.items():
-            value = view.kwargs.get(qs_param, query_paramas.get(qs_param, None))
-            filter_dict = {field_name: value}
-            if not secondary_donor:
-                secondary_donor = SecondaryDonor.objects.filter(**filter_dict).first()
-        return donor, secondary_donor
+    def _get_secondary_donor(self, view, query_params, user):
+        mapping_dict = {'id': 'secondary_donor_id', 'code': 'secondary_donor_code'}
+        return self._get_instance(view, query_params, user, mapping_dict, SecondaryDonor)
 
     def has_permission(self, request, view):
-        donor, secondary_donor = self.get_donors(view, request.query_params)
         user = request.user
+        donor = self._get_donor(view, request.query_params, user)
+        secondary_donor = self._get_secondary_donor(view, request.query_params, user)
         context_object = (donor, secondary_donor) if donor and secondary_donor else donor
         if donor and user.has_perm('roles.can_view_all_donors') or \
                 user.has_perm('report_metadata.view_donor', context_object):
