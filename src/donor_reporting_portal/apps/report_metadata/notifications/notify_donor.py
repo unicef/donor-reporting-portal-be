b5be94c31b523e7be3044d64cf33f5b8dd4ddf97
Domenico <dom.dinicola@gmail.com>
2022-01-25 16:58:06 -0600

    amend new report notifications

--- src/donor_reporting_portal/apps/report_metadata/notifications/notify_donor.py
+++ src/donor_reporting_portal/apps/report_metadata/notifications/notify_donor.py
@@ -3,7 +3,7 @@ from unicef_notification.utils import strip_text
 name = "notify_donor"
 defaults = {
     "description": "Notify Donor",
-    "subject": "Updated reports available in DRP",
+    "subject": "Updated reports available in UNICEF DRP",
     "content": strip_text("""Dear user,
 
     The following reports have been updated in the Donor Reporting Portal (drp.unicef.org).
@@ -14,8 +14,13 @@ defaults = {
         Grant: {{ report.grant_number }}
         Type: {{ report.report_type }}
         End date: {{ report.report_end_date|slice:"0:10" }}
-        
+
     {% endfor %}
+
+    You are receiving this system generated notification because of user setting in UNICEF’s Donor Reporting Portal.
+    In case you do not wish to receive this email or change the frequency, please contact your local site administrator.
+    Please do not reply. For any questions on the reports, please contact your UNICEF colleague.
+
     Regards.
     """),
     "html_content": """Dear user,<br/><br/>
@@ -24,16 +29,20 @@ defaults = {
     <br/><br/>
     <table>
     <tr>
+        <th>#</th>
         <th>Report</th>
         <th>Reference No</th>
+        <th>Category</th>
         <th>Grant No</th>
         <th>Report Type</th>
         <th>Report Date</th>
     </tr>
     {% for report in reports %}
         <tr>
+            <td>{{ forloop.counter }}</td>
             <td><a href="{{ report.download_url }}">{{ report.title }}</a></td>
             <td>{{ report.external_reference }}</td>
+            <td>{{ report.donor_report_category }}</td>
             <td>{{ report.grant_number }}</td>
             <td>{{ report.report_type }}</td>
             <td>{{ report.report_end_date|slice:"0:10" }}</td>
@@ -41,6 +50,16 @@ defaults = {
     {% endfor %}
     </table>
     <br/><br/>
+
+    <p>
+    You are receiving this system generated notification because of user setting in UNICEF’s Donor Reporting Portal.
+    </p>
+    <p>
+    In case you do not wish to receive this email or change the frequency, please contact your local site administrator.
+    </p>
+    <p>
+    Please do not reply. For any questions on the reports, please contact your UNICEF colleague.</p>
+
     Regards.
     """,
 }
