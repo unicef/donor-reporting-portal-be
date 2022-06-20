21b8fbf3fe618a30160864ed498352fa12fd7dc3
Domenico <dom.dinicola@gmail.com>
2022-06-20 22:50:45 +0200

    update config

--- src/donor_reporting_portal/api/views/config.py
+++ src/donor_reporting_portal/api/views/config.py
@@ -18,7 +18,8 @@ class ConfigAPIView(APIView):
                     'site_tracker': settings.MATOMO_SITE_TRACKER,
                     'site_id': settings.MATOMO_SITE_ID
                 },
-                'source_id': settings.DRP_SOURCE_IDS
+                'source_id': settings.DRP_SOURCE_IDS,
+                'gavi_donor_code': settings.GAVI_DONOR_CODE
             },
             status=status.HTTP_200_OK
         )
