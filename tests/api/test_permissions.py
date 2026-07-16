c661cdc608857d23c2f47abace182f4a3de37f2a
Domenico DiNicola <dom.dinicola@gmail.com>
2026-07-16 23:46:56 +0200

    fix: check permissions on all comma-separated donor_code values
    When donor_code contains multiple comma-separated values (e.g.
    C03001,C49902,C52501), split them and check the user has
    permission on at least one of the donors before granting access.

--- tests/api/test_permissions.py
+++ tests/api/test_permissions.py
@@ -5,8 +5,9 @@ from django.contrib.auth.models import AnonymousUser
 from rest_framework.request import Request
 from rest_framework.test import APIRequestFactory
 
+from factories import DonorFactory
 from donor_reporting_portal.api.permissions import DonorPermission, PublicLibraryPermission
-from perms import user_grant_permissions
+from perms import user_grant_permissions, user_grant_role_permission
 
 
 class TestDonorPermission:
@@ -36,6 +37,90 @@ class TestDonorPermission:
         request.user = user
         view = mock.Mock(kwargs={})
         assert perm.has_permission(request, view) is False
+
+    @pytest.mark.django_db
+    def test_single_donor_with_permission(self, user):
+        donor = DonorFactory(code="D001")
+        with user_grant_role_permission(user, donor, permissions=["report_metadata.view_donor"]):
+            perm = DonorPermission()
+            factory = APIRequestFactory()
+            request = Request(factory.get("/?donor_code=D001"))
+            request.user = user
+            view = mock.Mock(kwargs={})
+            assert perm.has_permission(request, view) is True
+
+    @pytest.mark.django_db
+    def test_single_donor_without_permission(self, user):
+        DonorFactory(code="D001")
+        perm = DonorPermission()
+        factory = APIRequestFactory()
+        request = Request(factory.get("/?donor_code=D001"))
+        request.user = user
+        view = mock.Mock(kwargs={})
+        assert perm.has_permission(request, view) is False
+
+    @pytest.mark.django_db
+    def test_comma_separated_donors_all_authorized(self, user):
+        donor1 = DonorFactory(code="D001")
+        donor2 = DonorFactory(code="D002")
+        donor3 = DonorFactory(code="D003")
+        with user_grant_role_permission(user, donor1, permissions=["report_metadata.view_donor"]):
+            with user_grant_role_permission(user, donor2, permissions=["report_metadata.view_donor"]):
+                with user_grant_role_permission(user, donor3, permissions=["report_metadata.view_donor"]):
+                    perm = DonorPermission()
+                    factory = APIRequestFactory()
+                    request = Request(factory.get("/?donor_code=D001,D002,D003"))
+                    request.user = user
+                    view = mock.Mock(kwargs={})
+                    assert perm.has_permission(request, view) is True
+
+    @pytest.mark.django_db
+    def test_comma_separated_donors_partial_authorized(self, user):
+        donor1 = DonorFactory(code="D001")
+        DonorFactory(code="D002")
+        DonorFactory(code="D003")
+        with user_grant_role_permission(user, donor1, permissions=["report_metadata.view_donor"]):
+            perm = DonorPermission()
+            factory = APIRequestFactory()
+            request = Request(factory.get("/?donor_code=D001,D002,D003"))
+            request.user = user
+            view = mock.Mock(kwargs={})
+            assert perm.has_permission(request, view) is True
+
+    @pytest.mark.django_db
+    def test_comma_separated_donors_none_authorized(self, user):
+        DonorFactory(code="D001")
+        DonorFactory(code="D002")
+        perm = DonorPermission()
+        factory = APIRequestFactory()
+        request = Request(factory.get("/?donor_code=D001,D002"))
+        request.user = user
+        view = mock.Mock(kwargs={})
+        assert perm.has_permission(request, view) is False
+
+    @pytest.mark.django_db
+    def test_comma_separated_donors_can_view_all(self, user):
+        DonorFactory(code="D001")
+        DonorFactory(code="D002")
+        DonorFactory(code="D003")
+        with user_grant_permissions(user, permissions=["roles.can_view_all_donors"]):
+            perm = DonorPermission()
+            factory = APIRequestFactory()
+            request = Request(factory.get("/?donor_code=D001,D002,D003"))
+            request.user = user
+            view = mock.Mock(kwargs={})
+            assert perm.has_permission(request, view) is True
+
+    @pytest.mark.django_db
+    def test_comma_separated_donors_nonexistent(self, user):
+        donor1 = DonorFactory(code="D001")
+        with user_grant_role_permission(user, donor1, permissions=["report_metadata.view_donor"]):
+            perm = DonorPermission()
+            factory = APIRequestFactory()
+            request = Request(factory.get("/?donor_code=D001,nonexistent"))
+            request.user = user
+            view = mock.Mock(kwargs={})
+            assert perm.has_permission(request, view) is True
 
 
 class TestPublicLibraryPermission:
